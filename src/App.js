6eef2e98a18d957d5ebbcea3c4da63709d5fba63
quarc0o <emmanuelq150@gmail.com>
2024-02-02 11:03:18 +0100

    Added aria-label

--- src/App.js
+++ src/App.js
@@ -37,6 +37,7 @@ function App() {
         <button
           type="submit"
           className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded transition-colors"
+          aria-label="Add Todo"
         >
           Add Todo
         </button>
